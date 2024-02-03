fdb8618db6213b8f238d539eac7853da6358cc36
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-02-03 14:42:27 +0200

    fix url v1.8

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -7,7 +7,7 @@ const ErrorPage: FC = () => {
     <div className="min-h-screen bg-slate-900 font-roboto text-white flex justify-center items-center flex-col gap-10">
       <img src={img} alt="img" className="w-80" />
       <Link
-        to={"/budgetTracker-frontend"}
+        to={"/"}
         className="bg-sky-500 rounded-md px-6 py-2 hover:bg-sky-600"
       >
         Back
