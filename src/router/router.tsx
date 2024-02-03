cc3c27c149600a2b715914d6877a6a98827eb7a2
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-02-03 15:00:14 +0200

    fix url v1.12

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -9,7 +9,7 @@ import ProtectedRoute from "../components/ProtectedRoute";
 
 export const router = createBrowserRouter([
   {
-    path: "/budgetTracker-frontend",
+    path: "/budgetTracker-frontend/",
     element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
