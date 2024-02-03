1bc24841cebaba20860432af54bf8e1ae2a3ad5d
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-02-03 15:53:36 +0200

    fix url v1.29

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/budgetTracker-frontend/",
+  base: "/budgetTracker-frontend",
   plugins: [react()],
 })
