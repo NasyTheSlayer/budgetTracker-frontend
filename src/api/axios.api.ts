038a65422b67d717f3059cdbc0f87cc5e4a5cbef
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-02-05 16:18:23 +0200

    fix API

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getTokenFromLocalStorage } from "../helpers/localstorage.helper";
 
 export const instance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://budgettracker-6jgk.onrender.com/api",
   headers: {
     Authorization: "Bearer " + getTokenFromLocalStorage() || "",
   },
