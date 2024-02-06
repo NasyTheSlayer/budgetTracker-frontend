9441cb11f86f77dab5fd38d8c2ecd81766a7487f
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2024-02-06 18:18:36 +0200

    fix errorpage

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,12 +1,8 @@
 import { FC } from "react";
 import img from "../assets/page_not_found.png";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectCount } from "../store/user/userSlice";
 
 const ErrorPage: FC = () => {
-  const { isAuth } = useSelector(selectCount);
-
   return (
     <div className="min-h-screen bg-slate-900 font-roboto text-white flex justify-center items-center flex-col gap-10">
       <img src={img} alt="img" className="w-80" />
